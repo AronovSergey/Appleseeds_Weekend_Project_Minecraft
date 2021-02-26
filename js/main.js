5f0d59a9e52b5ecf1b0eff3fa5ee972cd5738de2
Sergey <aronovsergeyy@gmail.com>
2021-02-26 15:30:08 +0200

    add remove element functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 /* 
-    sky, cloud, rock, land, fullLand, treeGreen and treeBrown is global variable created in the "elements.js"
+    ***sky, cloud, rock, land, fullLand, treeGreen and treeBrown is global variable created in the "elements.js"
+
+    ***elementEventListener is function located at mainButtonsActions.js
 */
 const board = document.querySelector('#board');
 
@@ -27,6 +29,8 @@ let boardBluePrint =
     ['fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl', 'fl'],
 ];
 
+let toolType;
+
 drawBoard();
 
 function drawBoard() {
@@ -36,25 +40,53 @@ function drawBoard() {
         for(let j = 0; j < 20; j++){
             switch (boardBluePrint[i][j]) {
                 case 's':
-                    board.appendChild(sky.cloneNode(true));
+                    const skyCopy = sky.cloneNode(true);
+                    skyCopy.setAttribute('indexI', i);
+                    skyCopy.setAttribute('indexJ', j);
+                    skyCopy.addEventListener('click', elementEventListener);
+                    board.appendChild(skyCopy);
                     break;
                 case 'l':
-                    board.appendChild(land.cloneNode(true));
+                    const landCopy = land.cloneNode(true);
+                    landCopy.setAttribute('indexI', i);
+                    landCopy.setAttribute('indexJ', j);
+                    landCopy.addEventListener('click', elementEventListener);
+                    board.appendChild(landCopy);
                     break;
                 case 'fl':
-                    board.appendChild(fullLand.cloneNode(true));
+                    const fullLandCopy = fullLand.cloneNode(true);
+                    fullLandCopy.setAttribute('indexI', i);
+                    fullLandCopy.setAttribute('indexJ', j);
+                    fullLandCopy.addEventListener('click', elementEventListener);
+                    board.appendChild(fullLandCopy);
                     break;
                 case 'c':
-                    board.appendChild(cloud.cloneNode(true));
+                    const cloudCopy = cloud.cloneNode(true);
+                    cloudCopy.setAttribute('indexI', i);
+                    cloudCopy.setAttribute('indexJ', j);
+                    cloudCopy.addEventListener('click', elementEventListener);
+                    board.appendChild(cloudCopy);
                     break;
                 case 'r':
-                    board.appendChild(rock.cloneNode(true));
+                    const rockCopy = rock.cloneNode(true);
+                    rockCopy.setAttribute('indexI', i);
+                    rockCopy.setAttribute('indexJ', j);
+                    rockCopy.addEventListener('click', elementEventListener);
+                    board.appendChild(rockCopy);
                     break;
                 case 'tg':
-                    board.appendChild(treeGreen.cloneNode(true));
+                    const treeGreenCopy = treeGreen.cloneNode(true);
+                    treeGreenCopy.setAttribute('indexI', i);
+                    treeGreenCopy.setAttribute('indexJ', j);
+                    treeGreenCopy.addEventListener('click', elementEventListener);
+                    board.appendChild(treeGreenCopy);
                     break;
                 case 'tb':
-                    board.appendChild(treeBrown.cloneNode(true));
+                    const treeBrownCopy = treeBrown.cloneNode(true);
+                    treeBrownCopy.setAttribute('indexI', i);
+                    treeBrownCopy.setAttribute('indexJ', j);
+                    treeBrownCopy.addEventListener('click', elementEventListener);
+                    board.appendChild(treeBrownCopy);
                     break;
                 default:
                     console.log("Wrong element type")
@@ -66,4 +98,14 @@ function drawBoard() {
 
 function insertShape(board, Shape, x, y){
 
+}
+
+function elementEventListener(e) {
+    if(this.classList.contains('removable')){
+        const i = this.getAttribute('indexI');
+        const j = this.getAttribute('indexJ');
+        boardBluePrint[i][j] = 's';
+        console.log(toolType);
+        drawBoard();
+    }
 }
--- js/mainButtonsActions.js
+++ js/mainButtonsActions.js
@@ -1,6 +1,12 @@
 const resetButton = document.querySelector('.sidebar__resetButton');
 const closeButton = document.querySelector('.closeButton');
-const axeButton = document.querySelector('axe');
+const axeButton = document.querySelector('.axe');
+const pickaxeButton = document.querySelector('.pickaxe');
+const shovelButton = document.querySelector('.shovel');
+
+
+
+const elementsArr = document.querySelectorAll('.element');
 
 resetButton.addEventListener('click', () => {
     window.location.reload()
@@ -11,5 +17,33 @@ closeButton.addEventListener('click', () => {
 });
 
 axeButton.addEventListener('click', () => {
-    type = 'axe';
+    removeStyleFromAllElements();
+    treeGreen.classList.add("removable");
+    treeBrown.classList.add("removable");
+    toolType = axeButton;
+    drawBoard();
 });
+
+pickaxeButton.addEventListener('click', () => {
+    removeStyleFromAllElements();
+    rock.classList.add("removable");
+    toolType = pickaxeButton;
+    drawBoard();
+});
+
+shovelButton.addEventListener('click', () => {
+    removeStyleFromAllElements();
+    fullLand.classList.add("removable");
+    land.classList.add("removable");
+    toolType = shovelButton;
+    drawBoard();
+});
+
+function removeStyleFromAllElements() {
+    treeGreen.classList.remove("removable");
+    treeBrown.classList.remove("removable");
+    rock.classList.remove("removable");
+    fullLand.classList.remove("removable");
+    land.classList.remove("removable");
+};
+
